a304e4aa25373a12bcac0c69f8652cd7590226cc
dheeraj kumar <dheerajchaudhary9401@gmail.com>
2026-02-02 22:50:12 +0530

    [Dheeraj Kumar] Add .Added csv data handling practice programs

---
+++ gcr-codebase/CSVHandling/ReadCSVFile.java
@@ -0,0 +1,21 @@
+package CSVHandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFile {
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader("CSVFileExample.csv"))){
+			String line;
+			while((line = br.readLine())!=null) {
+				System.out.println(line);
+			}
+		}catch(IOException e) {
+			System.out.println("file not found");
+		}
+
+	}
+
+}
